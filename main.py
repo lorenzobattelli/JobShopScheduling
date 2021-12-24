24270ac8f9f88ee7b993e249e78436b36503e8b6
Lorenzo Battelli <lorenzo.battelli96@gmail.com>
2021-12-24 23:17:35 +0100

    spostata solo una funzione

--- main.py
+++ main.py
@@ -320,10 +320,85 @@ def halt(k, maxiter):
     return k >= maxiter
 
 
+def find_best(p, search):
+    s = p.lista_soluzioni
 
+    k = 0
+    s.append(p.find_greedy_solution())
+    best = s[k]
+    if verbose:
+        print(BgColors.OKBLUE+"Soluzione di partenza")
+        print_soluzione(s[k].soluzione)
+        print("Costo: {}".format(s[k].makespan))
+        print("\nTabu list: {}\n".format(search.tabulist)+BgColors.ENDC)
 
+    while not halt(k, search.max_iter):
 
+        if verbose:
+            print(u'\u2500' * 100)
+            print("Iterazione", k+1)
+        
+        lista_ordinata = s[k].esplora_intorno()
+
+        for i in range(len(lista_ordinata)):
+            curr = lista_ordinata[i][0]
+            mossa = lista_ordinata[i][1]
+
+            if verbose:
+                print("Estraggo S_curr, con mossa {}:".format(mossa))
+                print_soluzione(curr.soluzione)
+                print("Costo: {}\n".format(curr.makespan))
+            
+            # criterio di aspirazione
+            if verbose:
+                print("Test su criterio di attivazione...")
+            if curr.getobjval() < best.getobjval(): # se la corrente è migliore dell'ottimo candidato
+                if verbose:
+                    print(BgColors.OKGREEN+"Successo! La soluzione S_curr diventa ottimo candidato! f(S_curr) < f(S_best)\nMi sposto su questa nuova soluzione\n"+BgColors.ENDC)
+                best = curr                         # aggiorno l'ottimo candidato
+                s.append(curr)                      # e mi sposto su questa soluzione
+                k += 1
+                # esco dal ciclo perchè ho trovato una soluzione migliore dell'ottimo candidato
+                # su cui mi sposto con sicurezza     
+                break
+            else:
+                if verbose:
+                    print(BgColors.FAIL+"Fallito. La soluzione S_curr non è migliore dell'ottimo candidato\n"+BgColors.ENDC)
+            
+            if verbose:
+                print("Stato della Tabulist: {}".format(search.tabulist))
+            if mossa not in search.tabulist:
+                if verbose:
+                    print(BgColors.OKGREEN+"La mossa {} NON è vietata dalla tabulist\nPosso spostarmi sulla soluzione generata da {}".format(mossa, mossa)+BgColors.ENDC)
+                s.append(curr)
+                if verbose:
+                    print("Aggiungo {} nella tabulist".format(inv(mossa)))
+                search.tabulist.append(inv(mossa))
+                if len(search.tabulist) > search.dim:
+                    forgotten = search.tabulist.pop(0)
+                    if verbose:
+                        print("Cancello dalla tabulist la mossa {}".format(forgotten))
+                k += 1
+                # posso eseguire la mossa perchè non è vietata, altrimenti di nuovo avrei dovuto 
+                # iterare per provare la prossima soluzione trovata dalla best
+                break
+            else:
+                if verbose:
+                    print("Non posso eseguire la mossa {} perché è vietata dalla tabulist\n".format(mossa))
+
+            if verbose:
+                print(BgColors.FAIL + "Seleziono la prossima soluzione utile\n"+BgColors.ENDC)
+
+        if verbose:
+            print("Tabulist aggiornata: {}\n".format(search.tabulist))
+
+    return best
+
+    
 class Problema:
+    
+
+
     ''' Istanza del problema, memorizza lista delle soluzioni trovate '''
 
     def __init__(self, n, m, macchine_associate, durate_ops):
@@ -516,80 +591,6 @@ class Soluzione:
 
         return lista_ordinata_fo
 
-
-def find_best(p, search):
-    s = p.lista_soluzioni
-
-    k = 0
-    s.append(p.find_greedy_solution())
-    best = s[k]
-    if verbose:
-        print(BgColors.OKBLUE+"Soluzione di partenza")
-        print_soluzione(s[k].soluzione)
-        print("Costo: {}".format(s[k].makespan))
-        print("\nTabu list: {}\n".format(search.tabulist)+BgColors.ENDC)
-
-    while not halt(k, search.max_iter):
-
-        if verbose:
-            print(u'\u2500' * 100)
-            print("Iterazione", k+1)
-        
-        lista_ordinata = s[k].esplora_intorno()
-
-        for i in range(len(lista_ordinata)):
-            curr = lista_ordinata[i][0]
-            mossa = lista_ordinata[i][1]
-
-            if verbose:
-                print("Estraggo S_curr, con mossa {}:".format(mossa))
-                print_soluzione(curr.soluzione)
-                print("Costo: {}\n".format(curr.makespan))
-            
-            # criterio di aspirazione
-            if verbose:
-                print("Test su criterio di attivazione...")
-            if curr.getobjval() < best.getobjval(): # se la corrente è migliore dell'ottimo candidato
-                if verbose:
-                    print(BgColors.OKGREEN+"Successo! La soluzione S_curr diventa ottimo candidato! f(S_curr) < f(S_best)\nMi sposto su questa nuova soluzione\n"+BgColors.ENDC)
-                best = curr                         # aggiorno l'ottimo candidato
-                s.append(curr)                      # e mi sposto su questa soluzione
-                k += 1
-                # esco dal ciclo perchè ho trovato una soluzione migliore dell'ottimo candidato
-                # su cui mi sposto con sicurezza     
-                break
-            else:
-                if verbose:
-                    print(BgColors.FAIL+"Fallito. La soluzione S_curr non è migliore dell'ottimo candidato\n"+BgColors.ENDC)
-            
-            if verbose:
-                print("Stato della Tabulist: {}".format(search.tabulist))
-            if mossa not in search.tabulist:
-                if verbose:
-                    print(BgColors.OKGREEN+"La mossa {} NON è vietata dalla tabulist\nPosso spostarmi sulla soluzione generata da {}".format(mossa, mossa)+BgColors.ENDC)
-                s.append(curr)
-                if verbose:
-                    print("Aggiungo {} nella tabulist".format(inv(mossa)))
-                search.tabulist.append(inv(mossa))
-                if len(search.tabulist) > search.dim:
-                    forgotten = search.tabulist.pop(0)
-                    if verbose:
-                        print("Cancello dalla tabulist la mossa {}".format(forgotten))
-                k += 1
-                # posso eseguire la mossa perchè non è vietata, altrimenti di nuovo avrei dovuto 
-                # iterare per provare la prossima soluzione trovata dalla best
-                break
-            else:
-                if verbose:
-                    print("Non posso eseguire la mossa {} perché è vietata dalla tabulist\n".format(mossa))
-
-            if verbose:
-                print(BgColors.FAIL + "Seleziono la prossima soluzione utile\n"+BgColors.ENDC)
-
-        if verbose:
-            print("Tabulist aggiornata: {}\n".format(search.tabulist))
-            
-    return best
 
 
 verbose = True
